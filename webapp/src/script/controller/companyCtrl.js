1c2a03f342dbefb4af0b1f9a5f10bd712e954a4a
rainbow <rainbow_qi@126.com>
2018-05-04 22:52:07 +0800

    解决一个小问题

--- webapp/src/script/controller/companyCtrl.js
+++ webapp/src/script/controller/companyCtrl.js
@@ -2,4 +2,4 @@
 
 angular.module('app').controller('companyCtrl', ['$scope', function ($scope) {
 
-}])
+}]);
--- webapp/src/script/controller/mainCtrl.js
+++ webapp/src/script/controller/mainCtrl.js
@@ -2,26 +2,7 @@
 
 angular.module('app').controller('mainCtrl', ['$http', '$scope', function ($http, $scope) {
      $http.get('/data/positionList.json').then(function (resp) {
-         console.log(111);
+         console.log(resp.data);
          $scope.list = resp.data;
      });
-    //$scope用来双向绑定
-   /* $scope.list1 = [{
-        id: '1',
-        name: 'web前端',
-        imgSrc: '/image/baidu.png',
-        companyName: '百度',
-        city: '北京',
-        industry: '互联网',
-        time: '2018-4-23 9:00'
-    }, {
-        id: '2',
-        name: 'web前端',
-        imgSrc: '/image/ali.png',
-        companyName: '阿里云',
-        city: '杭州',
-        industry: '互联网',
-        time: '2018-4-20 9:00'
-    }];
-*/
 }]);
