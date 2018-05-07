b5aae8834269c393e4ab4af32c0e45ece2e57ff4
rainbow <rainbow_qi@126.com>
2018-05-07 22:17:26 +0800

    完善逻辑

--- webapp/src/script/directive/position-class.js
+++ webapp/src/script/directive/position-class.js
@@ -4,9 +4,18 @@ angular.module('app').directive('appPositionClass', [function () {
     return {
         restrict: 'A',
         replace: true,
-        scope:{
-            com:'='
+        scope: {
+            com: '='
         },
-        templateUrl: 'view/template/position-class.html'
+        templateUrl: 'view/template/position-class.html',
+        link: function ($scope) {
+            $scope.showPositionList = function (index) {
+                $scope.positionList = $scope.com.positionClass[index].positionList;
+                $scope.isActive = index;
+            };
+            $scope.$watch('com', function (newVal) {
+                if (newVal) $scope.showPositionList(0);
+            })
+        }
     }
 }]);
